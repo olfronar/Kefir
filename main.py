6afd1fc887d3e74555b4bf6636eb8a0e9ab41ef9
Maxim Kruglikov <olfronar@gmail.com>
2012-03-01 15:35:22 +0400

    test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'olfronar'
+
+import os
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -10,20 +12,29 @@ from tornado.options import define, options
 define("port", default=8001, help="run on the given port", type=int)
 
 
-class MainHandler(tornado.web.RequestHandler):
+class Application(tornado.web.Application):
+    def __init__(self):
+        self.db = asyncmongo.Client(pool_id='mydb', host='127.0.0.1', port=27017, dbname='kefir_test')
+        handlers = [
+            (r'/', Main),
+            (r'/insert_data', Insert)
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+class Main(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render("main_template.html", title="My title")
 
+class Insert(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.application.db.messages.find({}, limit=50, sort=[('time', -1)], callback=self.on_response)
 
-def main():
-    tornado.options.parse_command_line()
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
-
-
-if __name__ == "__main__":
-    main()
+    def on_response(self, response, error):
+        if error:
+            raise tornado.web.HTTPError(500)
+        self.render('index.html', messages=response[::-1])
